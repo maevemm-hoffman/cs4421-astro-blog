75512f941700b21162081465f9b567377c25aec4
maevemm-hoffman <maevehoffman07@gmail.com>
2026-09-23 10:03:44 +0100

    feat(ci): configure linting tool

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+  {
+    ignores: [
+      ".astro/**",
+      "dist/**",
+      "node_modules/**",
+    ],
+  },
+
+  ...eslintPluginAstro.configs.recommended,
+
+  {
+    rules: {
+      // your custom rules here
+    },
+  },
+];
